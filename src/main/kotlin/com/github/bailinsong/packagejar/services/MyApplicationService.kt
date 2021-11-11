c73b8bced250fa069db4cf364a64656bb1a4d722
GitHub Action <action@github.com>
2021-11-11 07:47:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/bailinsong/packagejar/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bailinsong/packagejar/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bailinsong.packagejar.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bailinsong.packagejar.MyBundle
 
 class MyApplicationService {
 
